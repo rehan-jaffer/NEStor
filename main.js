784c72d0a0a8a719cc70877ce516b6557d7d8c6a
Rehan Jaffer <ray@thelondonvandal.com>
2018-05-04 16:56:45 +0100

    ability to read in ROM files

---
+++ main.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+
+class CPU {
+
+  constructor() {
+    this.memory = new Memory;
+    this .registers = {PC: 0, SP: 0};
+  }
+
+}
+
+class Memory {
+  constructor() {
+
+  }
+  fetch() {
+
+  }
+}
+
+class Emulator {
+  constructor() {
+    this.cpu = new CPU;
+  }
+  execute() {
+    let opcode = this.cpu.memory.fetch(this.cpu.registers.PC);
+  }
+}
+
+let emu = new Emulator;
+emu.execute();
+rom = fs.readFileSync("test.rom").toString('hex');
+console.log(rom);
