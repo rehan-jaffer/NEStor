b089d5af2eab9841db9b916a277ba9de393206e0
Rehan Jaffer <ray@thelondonvandal.com>
2018-06-07 17:28:12 +0100

    added more detailed test output

--- tests/test.js
+++ tests/test.js
@@ -8,33 +8,86 @@ var results = {A: 0, X: 0, Y:0, P: 0x24, SP: 0xFD};
 
 let cases = [];
 
-glob('cases/*.test', (err, files) => {
+class Tests {
+  constructor() {
 
-  files.map((file) => {
-    var text = fs.readFileSync(file);
-    cases.push(JSON.parse(text));
-  });
+  }
+  run() {
 
-  cases.forEach((c) => {
-    test(c["code"], c["registers"]);
-  });
+    let cases = this.load_tests();
 
-});
+    let results = cases.map((c) => {
+      return this.test(c);
+    });
 
-function test(instructions, output) {
 
-  let failed = false;
-  let emu = new emulator;
-  emu.direct_load(instructions);
-  emu.boot()
-  Object.keys(output).forEach((register) => {
-    if (emu.cpu.registers[register] == output[register]) {
-      console.log(`${register} [OK]`);
-    } else {
-      console.log(`${register} [FAIL]`);
-      failed = true;
-    }
-  });
-  return failed;
+    let passed = this.get_passing_count(results);
+    let failed = this.get_failing_summary(results);
+
+    console.log(`(${passed}/${results.length}) tests passed`);
+    console.log('Failing: ');
+    console.log(failed.join('\n'));
+
+  }
+
+  load_tests() {
+
+    var cases = [];
+    let files = glob.sync('cases/*.test');
+    files.forEach((file) => {
+      let text = fs.readFileSync(file);
+      cases.push(JSON.parse(text));
+    });
+
+    return cases;
+
+  }
+
+  get_failing_summary(results) {
+
+      return results.map((result) => {
+        return result['name'] + " [" + this.parse_results(result['results']) + "] "
+      });
+
+  }
+
+  get_passing_count(results) {
+
+      return results.map((result) => {
+        return result['status'] ? 1 : 0;
+      }).reduce((acc,x) => acc + x);
+
+  }
+
+  parse_results(registers) {
+    return Object.keys(registers).filter((r) => {
+      return !registers[r];
+    }).join(" ");
+  }
+
+  test(test_case) {
+
+    let failed = false;
+    let emu = new emulator;
+    let result = {};
+
+    emu.direct_load(test_case['code']);
+    emu.boot();
+
+    Object.keys(test_case['registers']).forEach((register) => {
+      if (emu.cpu.registers[register] == test_case['registers'][register]) {
+        result[register] = true;
+      } else {
+        result[register] = false;
+        failed = true;
+      }
+    });
+    console.log(result);
+    return {success: !failed, name: test_case['name'] || 'Unnamed test', results: result};
+
+  }
 
 }
+
+tests = new Tests;
+tests.run();
