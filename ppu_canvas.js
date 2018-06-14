dc392b2876cb13b5cb40289a1a9ebab7750cc6d4
Rehan Jaffer <ray@thelondonvandal.com>
2018-06-14 17:35:26 +0100

    dumping tiles as ascii now supported, onto canvas display next

--- ppu_canvas.js
+++ ppu_canvas.js
@@ -1,35 +1,54 @@
-var ppu = require('./PPU.js');
-var rom = require('./iNES.js');
+var ppu = require("./PPU.js");
+var rom = require("./iNES.js");
 
 let canvas = document.querySelector("#viewport");
 
-let p = new ppu;
+let p = new ppu();
 // let tables = p.dump_tables();
 
-let ines = new rom;
-var pattern_tables = [];
+let ines = new rom();
+var tiles = [];
 
 window.onload = function() {
-
-  alert("hi");
   let file_input = document.querySelector("#rom_file");
-  file_input.onchange = (self) => {
+  file_input.onchange = self => {
     let reader = new FileReader();
     let file = self.target.files[0];
-    reader.onload = (event) => {
+    reader.onload = event => {
       ines.load_data(event.target.result);
       p.load_rom(ines);
-      p.dump_tables();
+      p.get_tiles();
+      tiles = p.tiles;
+      draw_tiles(tiles);
     };
     reader.readAsArrayBuffer(file);
   };
-
 };
 
-function draw_bitmap(bitmap, canvas) {
-
-  
-
+function pad_byte(byte) {
+  return ("00000000" + byte.toString(2)).substr(-8);
 }
 
-console.log(tables);
+function highlight_bits(pattern) {
+  return pattern.replace(/1/gi, "<strong>1</strong>").replace(/0/gi, "&nbsp;");
+}
+
+function create_imagedata(tiles) {
+  for (let tile in tiles) {
+    for (let byte in tile) {
+    }
+  }
+}
+
+function draw_tiles(tiles) {
+  let t = "";
+
+  tiles.forEach(tile => {
+    tile.forEach(chunk => {
+      t += pad_byte(chunk[0] | chunk[1]);
+      t += "<br />";
+    });
+  });
+
+  document.querySelector("#tiles").innerHTML += highlight_bits(t);
+}
