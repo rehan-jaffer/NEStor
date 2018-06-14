9911847296c603ab9f420a9ecb949b45f8515636
Rehan Jaffer <ray@thelondonvandal.com>
2018-06-14 08:56:35 +0100

    dump_tables() is a really rough function for extracting tile data and outputting it to the console

--- PPU.js
+++ PPU.js
@@ -13,10 +13,31 @@
 class PPU {
   constructor() {
     this.registers = { ppu_ctrl: 0, ppu_mask: 0, ppu_status: 0, oam_addr: 0, oamdata: 0, ppu_scroll: 0, ppu_addr: 0, ppu_data: 0, oam_dma: 0 };
+    this.tiles = [];
   }
   load_rom(rom) {
-    this.rom = rom;
+    this.rom = rom.chr_rom;
+  }
+  dump_tables() {
+
+    let rom = this.rom;
+    let tables = [];
+    let chunk = 2;
+
+    for (let i = 0, j=rom.length;i<j;i+=chunk) {
+      tables.push(rom.slice(i, i+chunk));
+    }
+
+    for (let x = 0; x<tables.length; x++) {
+      for (let y = 0; y < 2; y++) {
+        console.log(("00000000" + tables[x][y].toString(2)).substr(-8));
+      }
+      console.log("");
+    }
+
   }
 }
+
+
 
 module.exports = PPU;
